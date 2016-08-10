a42f4ddd30b3ebbf5660122ad71f5083f5fd2128
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2016-08-10 05:43:38 -0300

    Updates to GO

--- lib/br_documents/documents/ie/go.rb
+++ lib/br_documents/documents/ie/go.rb
@@ -3,8 +3,10 @@ module BRDocuments
   class IE::GO < IE::Base
     set_verify_digits_weights first: %w(9 8 7 6 5 4 3 2)
 
-    set_format_regexp %r{^(\d{2})[.-]?(\d{3})[.-]?(\d{3})[.-]?(\d{1})}
+    set_format_regexp %r{^(10)[.-]?(\d{3})[.-]?(\d{3})[.-]?(\d{1})}
 
     set_pretty_format_mask %(%s.%s.%s-%s)
+
+    set_fixed_initial_numbers [1, 0]
   end
 end
