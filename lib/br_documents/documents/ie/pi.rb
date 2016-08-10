64d540dbe459538a4c543f5d25002883b5d18edb
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2016-08-10 05:26:32 -0300

    added tests for PI

--- lib/br_documents/documents/ie/pi.rb
+++ lib/br_documents/documents/ie/pi.rb
@@ -3,8 +3,10 @@ module BRDocuments
   class IE::PI < IE::Base
     set_verify_digits_weights first: %w(9 8 7 6 5 4 3 2)
 
-    set_format_regexp %r{^(\d{8})[-.]?(\d{1})}
+    set_format_regexp %r{^(19)[-.]?(\d{6})[-.]?(\d{1})}
 
-    set_pretty_format_mask %(%s-%s)
+    set_pretty_format_mask %(%s%s-%s)
+
+    set_fixed_initial_numbers [1, 9]
   end
 end
---
+++ spec/documents/ie/pi_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe BRDocuments::IE::PI do
+
+  it 'must generate root numbers with fixed numbers' do
+    number = described_class.generate_root_numbers # without formatting
+
+    initial_pos = described_class.initial_fix_numbers_position
+    fixed_numbers = described_class.fixed_initial_numbers
+
+    expect(number.slice(initial_pos, fixed_numbers.size)).to eq(fixed_numbers)
+  end
+
+  it 'generate documents must have valid fixed numbers' do
+    document = described_class.new(described_class.generate)
+
+    expect(document.valid_fixed_numbers?).to be_truthy
+  end
+
+  it 'must generate valid IE' do
+    10.times { expect(described_class.valid?(described_class.generate)).to be_truthy }
+  end
+
+  it 'pretty formats an IE number' do
+    expect(described_class.pretty("193013835")).to eq("19301383-5")
+  end
+
+  it 'must prettify IE number including initial fixed numbers' do
+    number = described_class.generate
+
+    expect(number).to match(/\A#{described_class.fixed_initial_numbers.join}/)
+  end
+
+  it 'remove an IE number formatting' do
+    expect(described_class.strip('19301383-5')).to eq('193013835')
+  end
+
+  it 'must validate correctly valid IE numbers' do
+    expect(described_class.valid?('19301383-5')).to be_truthy
+    expect(described_class.valid?('19-3013835')).to be_truthy
+    expect(described_class.valid?('193013835')).to be_truthy
+  end
+
+  it 'is invalid with wrong digits number' do
+    expect(described_class.invalid?('19301383-51')).to be_truthy
+  end
+
+  it 'must calculate verify digits' do
+    expected = [5]
+
+    expect(described_class.calculate_verify_digits("19301383-5")).to eq(expected)
+    expect(described_class.calculate_verify_digits("19301383")).to eq(expected)
+  end
+
+  it 'must validate a list of valid IE' do
+    numbers = [
+      '19301383-5', # COMPANHIA ENERGETICA DO PIAUI 08/08/1962
+      '19301656-7', # AGUAS E ESGOTOS DO PIAUI SA 28/01/1964
+      '19448642-7', # CARVALHO E FERNANDES LTDA
+      '19400969-6', # CLAUDINO S A LOJAS DE DEPARTAMENTOS
+      '19447385-6', # METROP0LITAN HOTEL LTDA
+      '19489563-7', # CEV V A P - CERAMICA VEREMLHA DO VALE DO PARNAIBA LTDA
+      '19451276-2', # DISTRIBUIDORA YORK LTDA
+      '19429717-9', # DISCAR DISTRIBUIDORA DE BEBIDAS CARVALHO LTDA
+    ].each do |number|
+      ie_object = described_class.new(number)
+
+      expect(ie_object).to be_valid
+      expect(ie_object).to_not be_invalid
+      expect(ie_object.pretty).to eq(number)
+      expect(ie_object.strip).to eq(described_class.strip(number))
+    end
+  end
+end
