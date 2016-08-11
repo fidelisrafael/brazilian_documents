f0d9aa3462125670b16d024bd684e0bfcefbe6a3
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2016-08-11 00:36:46 -0300

    Added tests to SE

--- lib/br_documents/documents/ie/se.rb
+++ lib/br_documents/documents/ie/se.rb
@@ -3,8 +3,10 @@ module BRDocuments
   class IE::SE < IE::Base
     set_verify_digits_weights first: %w(9 8 7 6 5 4 3 2)
 
-    set_format_regexp %r{^(\d{8})[-.]?(\d{1})}
+    set_format_regexp %r{^(27)(\d{3})[-.]?(\d{3})[-.]?(\d{1})}
 
-    set_pretty_format_mask %(%s-%s)
+    set_pretty_format_mask %(%s.%s.%s-%s)
+
+    set_fixed_initial_numbers [2, 7]
   end
 end
