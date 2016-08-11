9ac57856b5694ae2cef2419f36b6dad715d45c46
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2016-08-10 23:14:49 -0300

    Add RR specs

--- lib/br_documents/documents/ie/rr.rb
+++ lib/br_documents/documents/ie/rr.rb
@@ -5,16 +5,14 @@ module BRDocuments
 
     set_verify_digits_weights first: %w(1 2 3 4 5 6 7 8)
 
-    set_format_regexp %r{^(\d{8})[-.]?(\d{1})}
+    set_format_regexp %r{^(24)[-.]?(\d{6})[-.]?(\d{1})}
 
-    set_pretty_format_mask %(%s-%s)
+    set_pretty_format_mask %(%s.%s-%s)
 
     set_fixed_initial_numbers [2, 4]
 
-    class << self
-      def digit_verify(quotient_rest, division_factor)
-        quotient_rest
-      end
+    def calc_verify_digit(quotient_rest)
+      quotient_rest.to_i
     end
   end
 end
