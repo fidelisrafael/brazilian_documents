fbd2d6156ffb5c7a22a00034a4dc0484908d7341
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2020-11-09 16:12:04 -0300

    Revert "Allow IEs from DF to start with digits other than 07"

--- lib/br_documents/documents/ie/df.rb
+++ lib/br_documents/documents/ie/df.rb
@@ -6,11 +6,11 @@ module BRDocuments
                               last:  %w(5 4 3 2 9 8 7 6 5 4 3 2)
 
 
-    set_format_regexp %r{^(0\d)[-.]?(\d{3})[-.]?(\d{3})[\/-]?(\d{3})[-.]?(\d{2})}
+    set_format_regexp %r{^(07)[-.]?(\d{3})[-.]?(\d{3})[\/-]?(\d{3})[-.]?(\d{2})}
 
     set_pretty_format_mask %(%s.%s.%s.%s-%s)
 
-    set_fixed_digits [0]
+    set_fixed_digits [0, 7]
 
   end
 end
